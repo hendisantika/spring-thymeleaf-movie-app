4fdcabc4bbb008ce18f370223ad8f9da7706d2bb
Hendi Santika <hendi.santika@allianz.co.id>
2019-12-18 16:10:31 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springthymeleafmovieapp/SpringThymeleafMovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springthymeleafmovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafMovieAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafMovieAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springthymeleafmovieapp/SpringThymeleafMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springthymeleafmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafMovieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

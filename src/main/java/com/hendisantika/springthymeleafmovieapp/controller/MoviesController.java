c2a2739275c9b11f003f21a8a4d49f77b08640f7
Hendi Santika <hendi.santika@allianz.co.id>
2019-12-18 16:21:26 +0700

    #10 - Add movie API in MoviesController Class

--- src/main/java/com/hendisantika/springthymeleafmovieapp/controller/MoviesController.java
+++ src/main/java/com/hendisantika/springthymeleafmovieapp/controller/MoviesController.java
@@ -4,6 +4,9 @@ import com.hendisantika.springthymeleafmovieapp.repository.ActorRepository;
 import com.hendisantika.springthymeleafmovieapp.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +25,12 @@ public class MoviesController {
 
     @Autowired
     private ActorRepository actorRepository;
+
+    @GetMapping("/movie/{id}")
+    public String movie(@PathVariable Long id, Model model) {
+        model.addAttribute("movie", movieRepository.findById(id).get());
+        model.addAttribute("actors", actorRepository.findAll());
+        return "movie";
+    }
 }
+

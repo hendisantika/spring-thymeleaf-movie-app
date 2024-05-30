89d2560bb168e921060b0ae7b72fe5c7b3de09f8
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 06:11:51 +0700

    Bug Fix packages 🫸🌀✏️📗🐧🐳

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Spring Boot Thymeleaf Movie App
 
 ## Things To Do list:
-1. Install JDK 8 or JDK 11 on your machine
+
+1. Install JDK 8 or JDK 21 on your machine
 2. Install latest maven on your machine
-3. Clone this repository: `https://gitlab.com/hendisantika/spring-thymeleaf-movie-app.git`
+3. Clone this repository: `https://github.com/hendisantika/spring-thymeleaf-movie-app.git`
 4. Go to the folder: `cd spring-thymeleaf-movie-app`
 5. Run the application: `mvn clean spring-boot:run`
 6. Open your favorite browser: http://localhost:8080
--- src/main/java/com/hendisantika/springthymeleafmovieapp/domain/Movie.java
+++ src/main/java/com/hendisantika/springthymeleafmovieapp/domain/Movie.java
@@ -1,13 +1,13 @@
 package com.hendisantika.springthymeleafmovieapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import java.util.List;
 import java.util.Objects;
 
